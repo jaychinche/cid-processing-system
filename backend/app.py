e2cc6ad644de503003441c70c1d85ad445afd9a9
Jay chinche <94739010+jaychinche@users.noreply.github.com>
2025-06-10 22:22:07 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -665,7 +665,7 @@ def delete_collection(collection_name):
 if __name__ == '__main__':
     PORT=os.getenv('PORT')
     print(f"🚀 Flask Backend Running on http://127.0.0.1:{PORT}")
-    app.run(debug=True, port=10000)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 
